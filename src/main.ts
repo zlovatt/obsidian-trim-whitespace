cd322f696a258ac01141978a4b73bab99236adbe
Jon Erling Hustadnes <jonerling.hustadnes@proton.me>
2026-08-26 17:12:49 +0200

    refactor(trimDocument): now keeps line endings according to settings
    It now adds up to the minimum if they are missing and removed any above
    the maximum

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ import {
 import { TrimWhitespaceSettingTab } from "./settings";
 import handleTextTrim from "./utils/trimText";
 import getCursorFenceIndices from "./utils/getCursorFenceIndices";
+import { EOL } from "os";
 
 const DEFAULT_SETTINGS: TrimWhitespaceSettings = {
 	TrimOnSave: true,
@@ -290,9 +291,15 @@ export default class TrimWhitespace extends Plugin {
 			return;
 		}
 
-		editor.setValue(
-			trimmed + "\n".repeat(this.settings.TrailingLinesKeepMax),
-		);
+		const options = this.settings;
+
+		if (trimmed.length + options.TrailingLinesKeepMin >= input.length) {
+			trimmed += EOL.repeat(options.TrailingLinesKeepMin);
+		} else {
+			trimmed += EOL.repeat(options.TrailingLinesKeepMax);
+		}
+
+		editor.setValue(trimmed);
 		editor.setSelection(
 			editor.offsetToPos(newFromCursorOffset),
 			editor.offsetToPos(newToCursorOffset),
